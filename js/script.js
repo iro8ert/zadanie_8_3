7fb0b497a925bb07e3c75d979ce0c94494d346d3
Robert Nowaczyk <iro8ert@o2.pl>
2018-01-26 13:57:16 +0100

    add comments to script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+	var x = 5;
+	alert('Zadanie 8.3 z Java Scriptu'); //miałem skomentować tę linijkę zatem to robię. 
+}
+
+//zgodnie z poleceń dodaję jeszcze jeden komentarz liniowy
+
+/* a teraz dodam jeden krótki komentarz blokowy
+co może nie jest zgodne z dobrą praktyką ale to 
+tylko ćwiczenie jest*/
